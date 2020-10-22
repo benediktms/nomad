033d21505ca898a37d1324441c6bb4506c37da4a
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-10-22 10:38:50 +0100

    cleaning up index page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,12 +3,6 @@ import { BlitzPage } from "blitz"
 import { Box, Flex, Heading } from "@chakra-ui/core"
 
 import Layout from "app/layouts/Layout"
-import { Footer } from "../components/Footer"
-
-/*
- * This file is just for a pleasant getting started page for your new app.
- * You can delete everything in here and start from scratch if you like.
- */
 
 const Home: BlitzPage = () => {
   return (
@@ -16,7 +10,7 @@ const Home: BlitzPage = () => {
     <Flex direction="column" justifyContent="spaceBetween">
       <main>
         <Box>
-          <Heading>Welcome to nomad.</Heading>
+          <Heading textAlign="center">Hellow, World!</Heading>
         </Box>
       </main>
     </Flex>
