208287ff6495d282494269a40450c8274e34a45e
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-10-22 22:24:24 +0100

    implement react query tools

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,13 +1,14 @@
-import { AppProps, ErrorComponent, useRouter } from "blitz"
-import { ErrorBoundary, FallbackProps } from "react-error-boundary"
-import { queryCache } from "react-query"
-import LoginForm from "app/auth/components/LoginForm"
+import { AppProps, ErrorComponent, useRouter } from "blitz";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+import { queryCache } from "react-query";
+import LoginForm from "app/auth/components/LoginForm";
 
-import { ColorModeProvider, CSSReset, ThemeProvider } from "@chakra-ui/core"
+import { ColorModeProvider, CSSReset, ThemeProvider } from "@chakra-ui/core";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = Component.getLayout || ((page) => page)
-  const router = useRouter()
+  const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
 
   return (
     <ThemeProvider>
@@ -19,32 +20,33 @@ export default function App({ Component, pageProps }: AppProps) {
           onReset={() => {
             // This ensures the Blitz useQuery hooks will automatically refetch
             // data any time you reset the error boundary
-            queryCache.resetErrorBoundaries()
+            queryCache.resetErrorBoundaries();
           }}
         >
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-left" />
           {getLayout(<Component {...pageProps} />)}
         </ErrorBoundary>
       </ColorModeProvider>
     </ThemeProvider>
-  )
+  );
 }
 
 function RootErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
   if (error?.name === "AuthenticationError") {
-    return <LoginForm onSuccess={resetErrorBoundary} />
+    return <LoginForm onSuccess={resetErrorBoundary} />;
   } else if (error?.name === "AuthorizationError") {
     return (
       <ErrorComponent
         statusCode={(error as any).statusCode}
         title="Sorry, you are not authorized to access this"
       />
-    )
+    );
   } else {
     return (
       <ErrorComponent
         statusCode={(error as any)?.statusCode || 400}
         title={error?.message || error?.name}
       />
-    )
+    );
   }
 }
