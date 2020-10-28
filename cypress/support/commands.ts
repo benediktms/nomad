1977f19fe7eaf3cbb5935274a45b5c22659ba38b
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-10-28 18:20:01 +0000

    trying to get jest and cypress types to play nice

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,7 +24,6 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-// @ts-ignore
 Cypress.Commands.add("signup", ({ email, password }) => {
   cy.contains("button", "Sign Up").click();
 
